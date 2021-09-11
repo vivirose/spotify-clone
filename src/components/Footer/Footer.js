3108783f7babde061a549ef9b29baab46bf55436
vivirose <vivien.marcolin@gmail.com>
2021-09-10 19:15:29 -0500

    added audio player

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,10 @@
 import React from "react";
-import {
-    PlayCircleOutline,
-    SkipPrevious,
-    SkipNext,
-    PlaylistPlay,
-    Shuffle,
-    Repeat,
-    VolumeDown,
-  } from "@material-ui/icons";
-  import { Grid, Slider } from "@material-ui/core";
-  import cover from "./barbiegirl-cover.jpeg";
-  import "./Footer.css"
+import cover from "./barbiegirl-cover.jpeg";
+import "./Footer.css"
+import ReactAudioPlayer from 'react-audio-player';
 
   function Footer(){
+      const audioTag = React.useRef();
       return(
           <div className="footer">
               <div className="footerLeft">
@@ -23,9 +15,17 @@ import {
                   <h4>Barbie Girl</h4>
                   <p>Aqua</p>
                   </div>
-
               </div>
-
+            <div className="footerCenter">
+                <i className="material-icons">skip_previous</i>
+                <i className="material-icons">play_circle_outline</i>
+                <i className="material-icons">skip_next</i>
+                <ReactAudioPlayer
+                src="http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3"
+                autoPlay
+                controls
+                />  
+            </div>
           </div>
       )
   }
