87a980f1008e9717063f0fe20cb83c8f32a674e2
vivirose <vivien.marcolin@gmail.com>
2021-09-11 16:41:54 -0500

    adjusted footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,31 +3,40 @@ import cover from "./barbiegirl-cover.jpeg";
 import "./Footer.css"
 import ReactAudioPlayer from 'react-audio-player';
 
-  function Footer(){
-      const audioTag = React.useRef();
-      return(
-          <div className="footer">
-              <div className="footerLeft">
-                  <img src={cover}
-                  className="footer__albumCover"
-                  />
-                  <div className="footer__songInfo">
-                  <h4>Barbie Girl</h4>
-                  <p>Aqua</p>
-                  </div>
-              </div>
-            <div className="footerCenter">
-                <i className="material-icons">skip_previous</i>
-                <i className="material-icons">play_circle_outline</i>
-                <i className="material-icons">skip_next</i>
-                <ReactAudioPlayer
-                src="http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3"
-                autoPlay
-                controls
-                />  
-            </div>
-          </div>
-      )
-  }
+function Footer(){
 
-  export default Footer;
+    var music = new Audio("http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3");
+    var isPlaying = false;
+
+    function togglePlay() {
+        isPlaying ? music.pause() : music.play();
+    };
+    
+    music.onplaying = function() {
+        isPlaying = true;
+    };
+    music.onpause = function() {
+        isPlaying = false;
+    };
+    
+    return(
+        <div className="footer">
+            <div className="footer__left">
+                <img src={cover}
+                className="footer__albumCover"
+                />
+                <div className="footer__songInfo">
+                <h4>Barbie Girl</h4>
+                <p>Aqua</p>
+                </div>
+            </div>
+        <div className="footer__center">
+            <button><i className="material-icons">skip_previous</i></button>
+            <button onClick={togglePlay}><i className="material-icons">play_circle_outline</i></button>
+            <button><i className="material-icons">skip_next</i></button>
+        </div>
+        </div>
+    )
+}
+
+export default Footer;
